3737b9f7997d4597543dcefd08bc0c6cae215c73
Egor Kalinin <76444754+KaLiNkA-code@users.noreply.github.com>
2024-07-30 00:33:16 +0300

    x

--- part_2/testing_enviroment.js
+++ part_2/testing_enviroment.js
@@ -7,3 +7,35 @@ console.log("Hello", person)
 var greeting = "Hello"
 
 console.log(greeting, person)
+
+
+switch(greeting) {
+    case "Hello":
+        console.log("Drive");
+        break;
+    case "goodbay":
+        console.log("Drive");
+        break;
+    default:
+        //this block will run if no condition matches
+        console.log('There is no such day');
+}
+
+
+var example = Math.random()
+
+console.log("example", example)
+
+
+try {
+
+    
+    // console.log(a + b)
+    throw new ReferenceError;
+
+} catch (ReferenceError) {
+    console.log("Errr");
+}
+
+// throw new Error();
+// console.log("Hello");
