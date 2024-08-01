36bb15eede444d4e80a6360b5154089b5055ad37
Egor Kalinin <76444754+KaLiNkA-code@users.noreply.github.com>
2024-08-01 14:09:00 +0300

    ex

--- part_3/js/example.js
+++ part_3/js/example.js
@@ -18,3 +18,34 @@ exampleFunction(top3[0], top3[1], top3[2])
 console.log()
 
 exampleFunction(...top3)
+
+
+
+// const [] = top3;
+
+
+// const [] = top3;
+
+
+const fruits = ['apple', 'pear', 'plum']
+const berries = ['blueberry', 'strawberry']
+const fruitsAndBerries = [...fruits, ...berries] // concatenate
+console.log(fruitsAndBerries); // outputs a single array
+
+const flying = { wings: 2 }
+const car = { wheels: 4 }
+const flyingCar = {...flying, ...car}
+console.log(flyingCar) // {wings: 2, wheels: 4}
+
+
+console.log("###########################################")
+
+const meal = ["soup", "steak", "ice cream", "steak", "ice cream", "steak", "ice cream"]
+let [starter, second, third, ...exxx] = meal;
+console.log(starter);
+
+console.log(second);
+
+console.log(third);
+
+console.log(exxx);
